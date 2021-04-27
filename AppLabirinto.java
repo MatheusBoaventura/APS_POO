872c09951e206262f135c300e98c67214fadcfae
Matheus Boaventura Prieto <70549288+MatheusBoaventura@users.noreply.github.com>
2021-04-27 20:24:48 -0300

    Create AppLabirinto.java

---
+++ AppLabirinto.java
@@ -0,0 +1,17 @@
+public class AppLabirinto {
+    public static void main(String[] args) {
+        Meliante meliante = new Meliante();
+        Labirinto labirinto = new Labirinto();
+        labirinto.iniciarLab();
+        labirinto.imprimirLab();
+        System.out.println("\n procurando solucao \n");
+        int linhainicio = labirinto.linhaInicio();
+        int colunainicio = labirinto.colunainicio();
+        boolean achou = meliante.procurarCaminho(linhainicio, colunainicio);
+        if(achou){
+            System.out.println("achou caminho");
+        } else {
+            System.out.println("não tem caminho");
+        }
+    }
+}
